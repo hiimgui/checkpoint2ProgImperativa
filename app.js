18f522f481d2109cefbacb9204b3252165ad2e50
GuilhermeKageyama <guiproc@gmail.com>
2021-06-28 15:59:55 -0300

    refatorndo apos aula

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-
 let menu = {
     1 : {nome : "pipoca", duracao: 10 } ,
     2 : {nome: "macarrao", duracao: 8},
     3 : {nome: "carne", duracao: 15},
     4 : {nome: "feijao", duracao: 12},
-    5 : {nome: "brigadeiro", duracao:8},
-    duracaoArray : [8,10,12,15]
+    5 : {nome: "brigadeiro", duracao:8}
 }
 
-//calcular tempo total, levando em conta input cliente
+// levando em conta input cliente
 
-function microondas (prato, operacaoCliente, numeroDigitadoCliente) {
-    let tempoAposInputCliente;
-    //calcular tempo total
-    if(operacaoCliente === "+") {
-        tempoAposInputCliente = prato + numeroDigitadoCliente;
-    } else if (operacaoCliente === "-" ) {
-        tempoAposInputCliente = prato - numeroDigitadoCliente;
-    } 
-    //primeiro IF que valida o menu, ou ainda, o tempo padrao dos menus
-    if (prato === 8 || prato === 10 || prato === 12 || prato === 15){
-        if(tempoAposInputCliente < prato) {
+function microondas (numMenu, numeroDigitadoCliente) {
+    let duracao;
+    switch (numMenu) {
+        case 1:
+            duracao = 10;
+            break;
+        case 2:
+            duracao = 8;
+            break;
+        case 3:
+            duracao = 15;
+            break;
+        case 4:
+            duracao = 12;
+            break;
+        case 5:
+            duracao = 8;
+            break;
+
+        default: 
+            console.log("Prato Inexistente");
+            break;
+    }
+        if(numeroDigitadoCliente < duracao) {
             console.log("tempo insuficiente");
-        } else if(tempoAposInputCliente >= prato * 3 ) {
+        } else if(numeroDigitadoCliente >= duracao * 3 ) {
             console.log("KABUMMM");
-        } else if(tempoAposInputCliente > prato * 2 && tempoAposInputCliente < prato*3){
+        } else if(numeroDigitadoCliente > duracao * 2 && numeroDigitadoCliente < duracao*3){
             console.log("Comida Queimou");
-        } else {
+        } else if (numeroDigitadoCliente == duracao) {
             console.log("Prato pronto, Bom Apetite")
-        }
-    } else {
-        console.log("Prato inexistente")
-    }    
+        }  
 }
 
-microondas(12, "+", 13);
-microondas(menu[1].duracao)
-microondas(menu[3].duracao, "+", 50)
-microondas(5)
-microondas(menu[2].duracao, "-", 30)
-
-
+microondas(2, 8)
+microondas(1, 80)
+microondas(8, 80)
+microondas(2, 21)
